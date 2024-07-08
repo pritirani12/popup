a4c8b9d168bc789eaea5940c36a5939f72c0a7f1
pritirani12 <pr6243845@gmail.com>
2024-07-08 13:29:20 +0530

    first project
    first project in javascript

---
+++ js/script.js
@@ -0,0 +1,11 @@
+ let box = document.querySelector(".box");
+ //console.log("click");
+ 
+ function openPopup(){
+  box.classList.add("open-popup");
+ }
+
+
+ function closePopup(){
+  box.classList.remove("open-popup");
+ }
